febb3d7abc1148e0eecc16ca917f00f3ad95e1e4
David Augusto <davidmaciel178@gmail.com>
2024-08-30 00:42:37 -0300

    funcoes

--- JavaScript/js/function.js
+++ JavaScript/js/function.js
@@ -1,40 +1,26 @@
 window.onload = function(){
 
-    var num1, num2;
+  /*  var obj = {'nome': 'David', 'idade': 26, 'func': function(){console.log("David Matias")}}
 
-    num1 = 10;
-    num2 = 20;
+    obj.func();
+    
+
+    obj.func2 = function(){
+        console.log("Minha segunda função.")
+    }
+
+    obj.func2(); */
 
 
-        console.log("Operadores matematicos e lógicos");
-        console.log("Adição -> "+num1+" + "+num2+" = "+ (num1+num2));
-        console.log("Subtração -> "+num1+" - "+num2+" = "+ (num1-num2));
-        console.log("Multiplicação-> "+num1+" x "+num2+" = "+ (num1*num2));
-        console.log("Divisão -> "+num1+" / "+num2+" = "+ (num1/num2));
+    var variavel = [];
 
-        if(num1 === num2){
-            console.log("Se "+num1+" é maior que "+num2+" Esta condição é verdadeira");
-        } else {
-            console.log("Se "+num1+" é maior que "+num2+" Esta condição é falsa");
-        }
+    variavel[0] = [];
 
-        if(num1 == num2){
-            console.log("Se "+num1+" é igual "+num2+" Esta condição é verdadeira");
-        } else {
-            console.log("Se "+num1+" é igual "+num2+" Esta condição é falsa");
-        }
-        if(num1 != num2){
-            console.log("Se "+num1+" é diferente de "+num2+" Esta condição é verdadeira");
-        } else {
-            console.log("Se "+num1+" é diferente de "+num2+" Esta condição é falsa");
-        }
 
-        if(num1 !== num2){
-            console.log("Se "+num1+" não é semelhante que "+num2+" Esta condição é verdadeira");
-        } else {
-            console.log("Se "+num1+" não é semelhante que "+num2+" Esta condição é falsa");
-        }
+    variavel[0][2] = 'David';
 
-        console.log("Demais operadores são >= <= > <");
+    console.log(variavel[0][2]);
     
 }
+
+/*Parei bl 20 aula 09 1h21min
