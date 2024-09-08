4806282758cda76eae8771a57dcd31183be566d6
David Augusto <davidmaciel178@gmail.com>
2024-09-07 23:54:31 -0300

    Manipulando CSS via JQuery
    Manipulando CSS via JQuery

--- .vscode/JQuery/js/functions.js
+++ .vscode/JQuery/js/functions.js
@@ -1,8 +1,6 @@
-/*$(document).ready(function(){
-    alert("Olá Mundo!");
-})
-    */
-
 $(window).on('load', function(){
-    alert("Olá Mundo");
-})
+
+        $('.artigo1 > p').css('color','rgba(0,0,0,0.6)');
+        alert($('.artigo1 > p').css('color'));
+
+});
