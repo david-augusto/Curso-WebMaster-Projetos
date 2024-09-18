39c93be526d146203904f26d7df089aa085de42b
David Augusto <davidmaciel178@gmail.com>
2024-09-17 23:53:54 -0300

    atualizando

--- .vscode/JQuery/js/functions.js
+++ .vscode/JQuery/js/functions.js
@@ -1,14 +1,25 @@
 $(function(){
+
+        /*
         var el = $('.box');
 
-        var meutexto = 'Olá Mundo';
-        el.html('<div class="teste">Meu elemento</div>');
+        var meutexto = 'Olá Mundo!';
         el.html("<div class=\"teste\">"+meutexto+"</div>");
-
-        el.html(el.html() + '<h1 class="texto1">Meu texto via javascript</h1>')
+        el.html(el.html() + '<h1 class="texto1">Meu texto via javascript</h1>');
 
         $('.box2').text("<div></div>");
-        $('.box2').text("Olá mundo");
+        $('.box2').text($('.box2').text() + "Olá mundo");
 
         console.log($('.box2').text());
+        console.log($('.box').text());
+
+        $('input[type=text]').val("Olá Mundo");
+
+        $('textarea').text("Olá mundo");
+        */
+
+        $('input[type=button]').click(function(){
+                var v = $('input[type=text]').val();
+                console.log(v);
+        })
 });
