237c3a79a7d3b5be5911206078a430af8e8e4756
David Augusto <davidmaciel178@gmail.com>
2024-09-28 00:41:05 -0300

    Atualização
    Atualização

--- .vscode/JQuery/js/functions.js
+++ .vscode/JQuery/js/functions.js
@@ -1,37 +1,6 @@
 $(function(){
-
-        /*
-        var el = $('.box');
-
-        var meutexto = 'Olá Mundo!';
-        el.html("<div class=\"teste\">"+meutexto+"</div>");
-        el.html(el.html() + '<h1 class="texto1">Meu texto via javascript</h1>');
-
-        $('.box2').text("<div></div>");
-        $('.box2').text($('.box2').text() + "Olá mundo");
-
-        console.log($('.box2').text());
-        console.log($('.box').text());
-
-        $('input[type=text]').val("Olá Mundo");
-
-        $('textarea').text("Olá mundo");
-        */
-
-        $('input[type=button]').click(function(){
-
-              /*  var splitstr = str.split("@");
-
-                if(splitstr[1] == 'hotmail.com'){
-                        $('input[type=text]').css('opacity','0');
-                }else{
-                        console.log("A condição não bateu")
-                } */
-
-                  
-
-        })
-
         
+      var t = 'Meu contato'
 
-});
+      var t = 'Meu conteúdo após a tv';
+});;
