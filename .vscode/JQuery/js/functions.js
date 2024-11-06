b6a3ebcbb319086a0cd2d0fe27ac574d53687a5a
David Augusto <davidmaciel178@gmail.com>
2024-11-05 22:59:32 -0300

    Eventos
    Evento On

--- .vscode/JQuery/js/functions.js
+++ .vscode/JQuery/js/functions.js
@@ -1,23 +1,15 @@
 $(function(){
-      //funçãp append() para adicionar conteudo/elementos no final do elemento que selecionamos
-      //$('.box div').eq(0).append('<h3>Meu elemento alocado dinamicamente</h3>');
 
-    //  var el = $('<h3>Meu conteudo 3</h3>').appendTo($('.box'));
 
-    //  el.css('color','red');
+      $('body').on('click','a',function(){
+            $('body').css('background','black');
+            return false;
+      });
 
-     // $('.box').prepend('<h3>Olá Mundo!</h3>');
+      setTimeout(function(){
+            $('body').html('<a href="">Meu Link</a>');
+      },1000)
 
-     //var el = $('<h3>Meu conteudo 3</h3>').prependTo($('.box')).css('color','red');
+      
 
-    // var t = '<p>Meu conteudo após a div box</p>';
-      //$('.box').after(t);
-     //$('.box').before(t);
-     //$(t).insertAfter($('.box')).css('color','red');
-     //$(t).insertBefore($('.box')).css('color','red');
-
-     setTimeout(function(){
-            $('.box').eq(1).remove();
-      },3000);
-     
 });
