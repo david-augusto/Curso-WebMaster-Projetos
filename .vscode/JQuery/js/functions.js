90424ce285e9b0ebf5e45cd27c177f3d251e62be
David Augusto <davidmaciel178@gmail.com>
2024-11-12 01:08:37 -0300

    Efeitos e animações
    Efeitos e animações

--- .vscode/JQuery/js/functions.js
+++ .vscode/JQuery/js/functions.js
@@ -1,14 +1,15 @@
 $(function(){
 
-      $('.form_contato').submit(function(){
-            var container = $('.container');
-            var content = 'Nome: ' + $('input[name=nome]').val() +
-            '<hr>E-mail: '+ $('input[name=email]').val() +
-            '<hr>Telefone: '+$('input[name=telefone').val();
+      /*$('.box1').fadeOut(2000, function(){
+            $('.box2').fadeIn(1000, function(){
+                  console.log("Terminamos nossa animação");
+            })
+      });*/
 
-            container.html(content);
-
-            return false;
+      $('.box1').click(function(){
+            $('.box2').slideToggle(4000,function(){
+                  location.href="https://www.google.com"
+            });
       })
 
 });
