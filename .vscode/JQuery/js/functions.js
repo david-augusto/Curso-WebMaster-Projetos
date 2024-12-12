71edeaafd627be4673f16f28b5388206b9f8a8c6
David Augusto <davidmaciel178@gmail.com>
2024-12-12 00:44:27 -0300

    atualização

--- .vscode/JQuery/js/functions.js
+++ .vscode/JQuery/js/functions.js
@@ -1,19 +1,9 @@
 $(function(){
 
-      $('.box1').addClass('minhaclasse');
+     var el = $('#source .texto').clone();
 
-      //$('.minhaclasse').remove();
+     //$('#container').html(el.html());
 
-      $('.box1').removeClass('minhaclasse');
-
-      //$('.box1 > .child1 > .child2').css('color', 'purple');
-      //$('.box1').find('.child1').find('.child2').css('color','orange');
-
-      var el = $('.box1').find('.child1').find('.child2');
-      el.css('color','brown');
-
-      $('minhatag').attr('meuattr','outrovalor');
-
-      alert($('minhatag').attr('meuattr'));
+     $('#source .texto').appendTo('#container');
 
 });
